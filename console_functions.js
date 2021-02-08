7c976e58ae2ed908d37ef546ef73f2e71c2a8fc7
V M <sarcopter@yandex.ru>
2021-02-08 12:47:44 +0400

    function cs created v1.1.1

---
+++ console_functions.js
@@ -0,0 +1,19 @@
+
+
+
+// let _dev_cs = 'off'
+
+/*
+in _dev_cs can be disabled logs or 'off' for disable all
+ */
+// v1.1.1
+function cs(variable, symbol = '=', qty = 10) {
+    const func_name = Object.keys(variable)[0]
+    if (typeof _dev_cs !== 'undefined' && (_dev_cs[func_name] === false || _dev_cs === 'off')) {
+        return;
+    }
+    let filler = new Array(qty);
+    filler.fill(symbol);
+    console.log(filler.join('') + ' ' + func_name + ' ' + filler.join(''));
+    console.log(Object.values(variable)[0]);
+}
