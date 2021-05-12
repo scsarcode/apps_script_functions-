d58ab8dab0c5795c4a034a60ef8a6aee497030c5
V M <sarcopter@yandex.ru>
2021-05-12 13:03:12 +0400

    createArticle added

--- small_functions.js
+++ small_functions.js
@@ -9,3 +9,16 @@ function makeId(length=8) {
     }
     return result;
 }
+
+
+// Создает артикул из числа
+// num — номер в виде числа
+// pad — разрядность артикула
+// letter — префикс, который нужно добавить
+//v2.0.1
+function createArticle(num, pad=3, letter){
+    const str = String(num)
+    const article =  (letter ? letter + str.padStart((pad - letter.length), '0') : str.padStart(pad, '0'))
+    console.log(article)
+    return article
+}
